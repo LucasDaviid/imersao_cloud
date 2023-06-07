30eef5a768388b65ed46331414c03e19a48a041a
LucasDaviid <Lucass_David@outlook.com>
2023-06-07 19:16:43 -0300

    Informações do que foi feito no CloudShell

--- terraform_com_aws/main.tf
+++ terraform_com_aws/main.tf
@@ -18,3 +18,9 @@ resource "aws_s3_bucket" "s3_bucket" {
     bucket = "tcb-app-qa-jr10"
   
 }
+
+# Subimos o arquivo para o CloudShell
+# Lá utilizamos os comandos:
+#   terraform init - Provisiona a estrutura no CloudShell
+#   terraform plan - Compara o arquivo com o state file e com base nisso informa o que o terraform ira fazer na nuvem
+#   terraform apply - Confirma o que será aplicado na nuvem
